75a0a42bdc7086e3e64a160df89a865593c53ab8
mirfanm2019 <224185431+mirfanm2019@users.noreply.github.com>
2025-11-15 17:14:23 +0700

    Create LaporanGenerator.java
    Peran: Bertanggung jawab untuk mengumpulkan dan memformat data khusus untuk ditampilkan di bagian Laporan Keuangan dan di PDF.
    
    
    Tanggung Jawab: Berinteraksi dengan ManajerKeuangan untuk mendapatkan ringkasan data, seperti: total pemasukan, total pengeluaran, saldo akhir, dan status pengeluaran (Aman/Tidak Aman ).

---
+++ LaporanGenerator.java
@@ -0,0 +1,2 @@
+//Peran: Bertanggung jawab untuk mengumpulkan dan memformat data khusus untuk ditampilkan di bagian Laporan Keuangan dan di PDF.
+//Tanggung Jawab: Berinteraksi dengan ManajerKeuangan untuk mendapatkan ringkasan data, seperti: total pemasukan, total pengeluaran, saldo akhir, dan status pengeluaran (Aman/Tidak Aman ).
