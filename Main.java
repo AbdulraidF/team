de1fa7722d8be79ebdd9f4a32bb93f360f3aaca7
mirfanm2019 <224185431+mirfanm2019@users.noreply.github.com>
2025-11-17 18:34:43 +0700

    Update Main.java
    Aku izin ubah sesuai design zoom kemaren

--- Main.java
+++ Main.java
@@ -1,20 +1,16 @@
 package KeuanganApp;
 
-import javax.swing.SwingUtilities;
-
 public class Main {
     public static void main(String[] args) {
-        // 1. Tampilkan Splash Screen di Thread yang berbeda (Opsional, tapi lebih baik)
-        // Atau langsung di main thread sebelum memuat aplikasi utama.
-
-        SplashScreen splash = new SplashScreen();
-        splash.showSplash(); // Tampilkan dan tunggu 3 detik
-
-        // 2. Setelah splash screen selesai, jalankan GUI utama di Event Dispatch Thread (EDT)
-        SwingUtilities.invokeLater(new Runnable() {
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
-                new AplikasiGUI(); // Aplikasi utama dimuat
+                AplikasiGUI gui =  new AplikasiGUI();
+
+                gui.setVisible(true);
             }
         });
     }
 }
+
+
+
